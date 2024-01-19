5b9808f306e7e1a658d2845c8f371ae2f966255c
Your Name <you@example.com>
2024-01-19 22:51:05 +0300

    first

---
+++ task1.js
@@ -0,0 +1,5 @@
+let str = "Я изучаю JavaScript";
+console.log(str[2]);
+console.log(str[0]);
+console.log(str[str.length - 1]);
+
---
+++ task2.js
@@ -0,0 +1,5 @@
+let str="Я изучаю JavaScript ";
+let newstr=str.replace("ю", "1");
+console.log(newstr);
+let newnewstr = str.replace("JavaScript", "Frontend");
+console.log(newnewstr);
