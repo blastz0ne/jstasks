80bb2ebce7d7447241705d609da65c7491e1521e
blastz0ne <150605466+blastz0ne@users.noreply.github.com>
2024-01-19 23:07:21 +0300

    Delete task5.js

--- task5.js
+++
@@ -1,8 +0,0 @@
-let arr = [31, 10, 'chicken', 9, 'fish', 10];
-let stringArray = arr.filter(element => typeof element === 'string');
-console.log(stringArray);
-//массив с элементами, равными 10
-let arr = [31, 10, 'chicken', 9, 'fish', 10];
-let numberTenArray = arr.filter(element => element === 10);
-console.log(numberTenArray);
-
