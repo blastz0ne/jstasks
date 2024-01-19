5b6d1bbfc56cfd2308c29ddfff773987064c6ac5
blastz0ne <150605466+blastz0ne@users.noreply.github.com>
2024-01-19 23:07:14 +0300

    Delete task4.js

--- task4.js
+++
@@ -1,7 +0,0 @@
-let a = ['cat', 'dog', 'parrot', 'horse', 'fish', 'chicken', 'lion'];
-let indexOfFish = a.indexOf('fish');
-a.splice(indexOfFish, 1);
-let indexOfCat = a.indexOf('cat');
-a.splice(indexOfCat, 1);
-a.splice(-2);
-console.log(a);
